891f95095fed388f22d700492946b553fdac18e3
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-02-19 13:09:39 -0600

    Add Details Game Call

--- app/src/main/java/com/gklyphon/gameinsight/services/IRetrofitService.kt
+++ app/src/main/java/com/gklyphon/gameinsight/services/IRetrofitService.kt
@@ -1,8 +1,11 @@
 package com.gklyphon.gameinsight.services
 
+import com.gklyphon.gameinsight.models.Game
+import com.gklyphon.gameinsight.models.GameDetail
 import com.gklyphon.gameinsight.models.GameResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -22,5 +25,11 @@ interface IRetrofitService {
         @Query("key") apiKey: String,
         @Query("page") page: Int = 1
     ): Call<GameResponse>
+
+    @GET("games/{id}")
+    fun getGameDetails(
+        @Path("id") id: Int,
+        @Query("key") apiKey: String
+    ): Call<GameDetail>
 
 }
